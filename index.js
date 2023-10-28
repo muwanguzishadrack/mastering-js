d1912f8a905e12705b7257d09bd8c5530cb5e68e
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-10-28 10:55:05 +0300

    Calculation function

--- index.js
+++ index.js
@@ -50,14 +50,14 @@
 
 // FUNCTIONS
 // Performing a task
-function greet(name, lastName) {
-    console.log('Hello', name, lastName);
-}
+// function greet(name, lastName) {
+//     console.log('Hello', name, lastName);
+// }
 
 // // Calculates a value
 
-// function square(number) {
-//     return number * number;
-// }
+function square(number) {
+    return number * number;
+}
 
-// console.log(square(2));
+console.log(square(2));
