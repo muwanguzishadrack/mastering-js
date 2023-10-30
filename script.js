d2caa5b5b05bc7495270258f2f563fd4c27ad15e
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-10-30 22:19:15 +0300

    let, const and var

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+/*
 // VARIABLES
-// let js = "amazing";
-// console.log(4 + 8 + 12 - 10);
+let js = "amazing";
+console.log(4 + 8 + 12 - 10);
 
-// console.log("Shadrack");
-// console.log(23);
+console.log("Shadrack");
+console.log(23);
 
 let firstName = "Shadrack";
 console.log(firstName);
@@ -15,3 +16,47 @@ let myFirstJob = "Programmer";
 let myCurrentJob = "Teacher";
 
 console.log(myFirstJob);
+
+DATA TYPES
+1. Numbers
+2. Strings
+3. Boolean
+4. Undefined
+5. Null
+6. Symbol
+7. BigInt
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Shadrack");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+// let, CONST AND VAR
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+// lastName = "Shadrack";
+// console.log(lastName);
