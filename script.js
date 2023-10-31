f276a8a68748485fb58d409aef7bce87187dd84b
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-10-31 17:35:04 +0300

    Coding Exercise 4: CHALLENGE #4 Solution

--- script.js
+++ script.js
@@ -392,7 +392,8 @@ if (age >= 18) {
 console.log(drink2);
 
 console.log(`I like to drink ${age >= 23 ? "wine" : "water"}`);
-*/
+
+
 // Coding Exercise 4: CHALLENGE #4
 // CHALLENGE #4
 // Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
@@ -409,9 +410,10 @@ console.log(`I like to drink ${age >= 23 ? "wine" : "water"}`);
 
 // TEST DATA: Test with different bill values: 275, 40, and 430
 
-const bill = 275;
+const bill = 430;
 
 const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 console.log(
   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
 );
+*/
