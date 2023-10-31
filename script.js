7f1e9b9b02eac5ba75b1d83271831e140f82e028
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-10-31 22:13:01 +0300

    FUNCTIONS

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ if (hasDriversLicense) console.log("I can drive :D");
 
 // const interface = "Audio";
 // const private = 534;
-*/
 
 // Functions:
 
@@ -31,3 +30,22 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+const num = Number("23");
+*/
+
+// FUNCTIONS
+//Function Declarations
+
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+
+//Function Expressions
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
