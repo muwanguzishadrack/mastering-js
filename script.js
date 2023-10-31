0ab90c91437e0ae45a30bcf463a05c3c465ce0f8
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-10-31 12:42:49 +0300

    TRUTHY AND FALSY VALUES

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ if (BMIMark > BMIJohn) {
     `John's BMI (${BMIJohn}) is higher than Mark's BMI (${BMIMark})!`
   );
 }
-*/
+
 
 // TYPE COVERSION
 const inputYear = "1991";
@@ -207,3 +207,26 @@ console.log("23" * "2");
 let n = "1" + 1; // 11
 n = n - 1; // 11 - 1
 console.log(n);
+*/
+
+//TRUTHY AND FALSY VALUES
+// 5 falsy values: 0, '', undefined, null. NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all");
+} else {
+  console.log("You should get a job!");
+}
+
+let height = 0;
+if ((height = 0)) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
