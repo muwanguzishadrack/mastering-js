99d7173295549f66d8a0a043ac13ef91d519cd70
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-10-31 16:13:50 +0300

    Boolean Logic

--- script.js
+++ script.js
@@ -257,3 +257,26 @@ if (favorite !== 23) console.log("Why not 23?");
 */
 
 // Boolean Logic
+// Boolean Logic operators: and, or, not
+
+const hasDriversLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log("Sarah is able to drive!");
+// } else {
+//   console.log("Someone else should drive");
+// }
+
+const isTired = true;
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log("Sarah is able to drive!");
+} else {
+  console.log("Someone else should drive");
+}
