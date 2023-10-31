22caaa3a1d08c868e73ca6d984e9e71f6ec76e8f
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-10-31 16:34:37 +0300

    Coding Exercise 3: CHALLENGE #3 Bonus

--- script.js
+++ script.js
@@ -254,7 +254,7 @@ if (favorite === 23) {
 }
 
 if (favorite !== 23) console.log("Why not 23?");
-*/
+
 
 // Boolean Logic
 // Boolean Logic operators: and, or, not
@@ -280,3 +280,52 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
   console.log("Someone else should drive");
 }
+
+//Coding Exercise 3: CHALLENGE #3
+
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log("Dolphins win the trophy");
+// } else if (scoreKoalas > scoreDolphins) {
+//   console.log("Koalas win the trophy");
+// } else if (scoreKoalas === scoreDolphins) {
+//   console.log("Both win the trophy");
+// }
+
+// BONUS 1
+
+// const scoreDolphins = (97 + 112 + 101) / 3;
+// const scoreKoalas = (109 + 95 + 123) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+//   console.log("Dolphins win the trophy");
+// } else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+//   console.log("Koalas win the trophy");
+// } else if (scoreKoalas === scoreDolphins) {
+//   console.log("Both win the trophy");
+// }
+
+// BONUS 2
+
+const scoreDolphins = (97 + 112 + 80) / 3;
+const scoreKoalas = (109 + 95 + 50) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log("Dolphins win the trophy");
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+  console.log("Koalas win the trophy");
+} else if (
+  scoreKoalas === scoreDolphins &&
+  scoreDolphins >= 100 &&
+  scoreKoalas >= 100
+) {
+  console.log("Both win the trophy");
+} else {
+  console.log("No one wins the trophy");
+}
+*/
