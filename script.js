20b6f612e4bfdbe69da94af1decf2ed04d65f73a
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-10-31 22:02:04 +0300

    Functions

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+// const interface = "Audio";
+// const private = 534;
+*/
+
+// Functions:
+
+function logger() {
+  console.log("My name is Shadrack");
+}
+
+// Calling/ running / invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
