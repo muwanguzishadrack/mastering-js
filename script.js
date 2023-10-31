6d22cb0011c6059b127ddf95b2061f8e934694dd
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-10-31 16:51:52 +0300

    // The switch Statement

--- script.js
+++ script.js
@@ -329,3 +329,45 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
   console.log("No one wins the trophy");
 }
 */
+
+// The switch Statement
+
+const day = "sunday";
+
+switch (day) {
+  case "monday": // day === monday
+    console.log("Plan my course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weeked :D");
+    break;
+  default:
+    console.log("Not a valid day!");
+}
+
+if (day === "monday") {
+  console.log("Plan my course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy the weeked :D");
+} else {
+  console.log("Not a valid day!");
+}
