5b4daa4494b52109de54c5cdbb50baa1510ffebc
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-10-31 17:28:39 +0300

    Coding Exercise 4: CHALLENGE #4

--- script.js
+++ script.js
@@ -371,7 +371,7 @@ if (day === "monday") {
 } else {
   console.log("Not a valid day!");
 }
-*/
+
 // The Conditional (Ternary) Operator
 
 const age = 23;
@@ -392,3 +392,26 @@ if (age >= 18) {
 console.log(drink2);
 
 console.log(`I like to drink ${age >= 23 ? "wine" : "water"}`);
+*/
+// Coding Exercise 4: CHALLENGE #4
+// CHALLENGE #4
+// Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+// Your tasks:
+
+// Calculate the tip, depending on the bill value. Create a variable called tip for this. It's not allowed to use an if...else statement (if it's easier for you, you can start with an if...else statement, and then try to convert it to a ternary operator).
+
+// Print a string to the console containing the bill value, the tip, and the final value (bill + tip).
+
+// Example: The bill was 275, the tip was 41.25, and the total value 316.25.
+
+// Note: Use the values of the bill and tip variables to construct this string. Don't hard-code them 🙂
+
+// TEST DATA: Test with different bill values: 275, 40, and 430
+
+const bill = 275;
+
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
