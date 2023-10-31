b4f00940b321b20c98988dc7980e9c07988469f9
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-10-31 12:31:22 +0300

    TYPE COVERSION

--- script.js
+++ script.js
@@ -188,3 +188,22 @@ if (BMIMark > BMIJohn) {
   );
 }
 */
+
+// TYPE COVERSION
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Shadrack"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// TYPE COERCION
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" * "2");
+
+let n = "1" + 1; // 11
+n = n - 1; // 11 - 1
+console.log(n);
