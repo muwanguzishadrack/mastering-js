b374c6d4fefa6aaa32e0f4327ec27b8bf4650d26
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-11-01 09:53:34 +0300

    Arrow Functions

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number("23");
-*/
+
 
 // FUNCTIONS
 //Function Declarations
@@ -49,3 +49,19 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+*/
+
+//Arrow Functions
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Shadrack"));
+console.log(yearsUntilRetirement(1980, "Bob"));
