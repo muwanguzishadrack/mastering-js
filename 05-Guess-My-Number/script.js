18c0257404da9b0b40ceb444788d5fe9a0344f36
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-11-06 08:16:27 +0300

    Selecting and Manipulation elements in DOM

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,6 +1,11 @@
 'use strict';
 
-// Query Selector
+// Dom (Document Object Model: Connection between JS and HTML
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
 
-// Dom and Dom Manipulation
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23; // Use value for input fields
+console.log(document.querySelector('.guess').value);
