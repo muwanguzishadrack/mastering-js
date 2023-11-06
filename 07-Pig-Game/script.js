d8d501c7d566c59faccabec12565c895b0768e72
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-11-06 19:29:05 +0300

    Rolling the dice functionality

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,12 +1,38 @@
 'use strict';
 
-// Selecting elemes
+// Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Resetting scores to o
 score0El.textContent = 0;
 score1El.textContent = 0;
 // Adding hiddent element on dice class to hide it at the start
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling the dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // console.log(dice);
+  // 2. Display Dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // CHANGE LATER
+  } else {
+    // Switch to next player
+  }
+});
