ee6d0a591b93c9230b94b527052df77a95c6fd44
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-11-06 19:05:26 +0300

    Adding hiddent element on dice

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// Selecting elemes
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Resetting scores to o
+score0El.textContent = 0;
+score1El.textContent = 0;
+// Adding hiddent element on dice class to hide it at the start
+diceEl.classList.add('hidden');
