793ba5aab43871c3f2334a8b1b4f6649eaa655e5
muwanguzishadrack <muwanguzishadrackm@gmail.com>
2023-11-06 10:40:42 +0300

    Implimenting Game Logic

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // Dom (Document Object Model: Connection between JS and HTML
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number!';
@@ -9,3 +9,48 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23; // Use value for input fields
 console.log(document.querySelector('.guess').value);
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    // When there is no input
+    document.querySelector('.message').textContent = 'No Number!';
+
+    // When the player wins
+  } else if (guess == secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess it too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
